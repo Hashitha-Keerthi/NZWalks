4cd9b8c3231b2c408e93577d81ba2389a6ab1ba6
Hashitha Keerthi <hkeerthi@icptechno.com>
2023-07-25 11:35:13 +0530

    AddRegionDto

--- NZWalksAPI/Models/DTO/AddRegionDto.cs
+++ NZWalksAPI/Models/DTO/AddRegionDto.cs
@@ -4,6 +4,7 @@ namespace NZWalksAPI.Models.DTO
 {
     public class AddRegionDto
     {
+     
         [Required]
         [MinLength(3, ErrorMessage = "Code has to be minimum of 3 characters")]
         [MaxLength(3, ErrorMessage = "Code has to be maximum of 3 characters")]
